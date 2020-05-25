a3849a82f57b10daaec724b2f15756775dfa98fb
Bill Hewlett <bill@imagecue.lighting>
2020-05-25 09:33:51 -0700

    added E-Touch panel

---
+++ drivers/gpu/drm/panel/panel-etouch-jd9365.c
@@ -0,0 +1,621 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) ImageCue LLC 2020
+ *
+ *
+ * Author:  Bill Hewlett <bill@imagecue.lighting>
+ */
+
+#include <linux/backlight.h>
+#include <linux/gpio/consumer.h>
+#include <linux/regulator/consumer.h>
+
+#include <video/mipi_display.h>
+
+#include <drm/drmP.h>
+#include <drm/drm_mipi_dsi.h>
+#include <drm/drm_panel.h>
+
+/*** Manufacturer Command Set ***/
+#define MCS_CMD_MODE_SW		0xFE /* CMD Mode Switch */
+#define MCS_CMD1_UCS		0x00 /* User Command Set (UCS = CMD1) */
+#define MCS_CMD2_P0		0x01 /* Manufacture Command Set Page0 (CMD2 P0) */
+#define MCS_CMD2_P1		0x02 /* Manufacture Command Set Page1 (CMD2 P1) */
+#define MCS_CMD2_P2		0x03 /* Manufacture Command Set Page2 (CMD2 P2) */
+#define MCS_CMD2_P3		0x04 /* Manufacture Command Set Page3 (CMD2 P3) */
+
+/* CMD2 P0 commands (Display Options and Power) */
+#define MCS_STBCTR		0x12 /* TE1 Output Setting Zig-Zag Connection */
+#define MCS_SGOPCTR		0x16 /* Source Bias Current */
+#define MCS_SDCTR		0x1A /* Source Output Delay Time */
+#define MCS_INVCTR		0x1B /* Inversion Type */
+#define MCS_EXT_PWR_IC		0x24 /* External PWR IC Control */
+#define MCS_SETAVDD		0x27 /* PFM Control for AVDD Output */
+#define MCS_SETAVEE		0x29 /* PFM Control for AVEE Output */
+#define MCS_BT2CTR		0x2B /* DDVDL Charge Pump Control */
+#define MCS_BT3CTR		0x2F /* VGH Charge Pump Control */
+#define MCS_BT4CTR		0x34 /* VGL Charge Pump Control */
+#define MCS_VCMCTR		0x46 /* VCOM Output Level Control */
+#define MCS_SETVGN		0x52 /* VG M/S N Control */
+#define MCS_SETVGP		0x54 /* VG M/S P Control */
+#define MCS_SW_CTRL		0x5F /* Interface Control for PFM and MIPI */
+
+/* CMD2 P2 commands (GOA Timing Control) - no description in datasheet */
+#define GOA_VSTV1		0x00
+#define GOA_VSTV2		0x07
+#define GOA_VCLK1		0x0E
+#define GOA_VCLK2		0x17
+#define GOA_VCLK_OPT1		0x20
+#define GOA_BICLK1		0x2A
+#define GOA_BICLK2		0x37
+#define GOA_BICLK3		0x44
+#define GOA_BICLK4		0x4F
+#define GOA_BICLK_OPT1		0x5B
+#define GOA_BICLK_OPT2		0x60
+#define MCS_GOA_GPO1		0x6D
+#define MCS_GOA_GPO2		0x71
+#define MCS_GOA_EQ		0x74
+#define MCS_GOA_CLK_GALLON	0x7C
+#define MCS_GOA_FS_SEL0		0x7E
+#define MCS_GOA_FS_SEL1		0x87
+#define MCS_GOA_FS_SEL2		0x91
+#define MCS_GOA_FS_SEL3		0x9B
+#define MCS_GOA_BS_SEL0		0xAC
+#define MCS_GOA_BS_SEL1		0xB5
+#define MCS_GOA_BS_SEL2		0xBF
+#define MCS_GOA_BS_SEL3		0xC9
+#define MCS_GOA_BS_SEL4		0xD3
+
+/* CMD2 P3 commands (Gamma) */
+#define MCS_GAMMA_VP		0x60 /* Gamma VP1~VP16 */
+#define MCS_GAMMA_VN		0x70 /* Gamma VN1~VN16 */
+
+struct jd9366 {
+	struct device *dev;
+	struct drm_panel panel;
+	struct gpio_desc *reset_gpio;
+	struct regulator *supply;
+	struct backlight_device *backlight;
+	bool prepared;
+	bool enabled;
+};
+//E-Touch panel
+static const struct drm_display_mode default_mode = {
+	.clock = 70794,
+	.hdisplay = 800,
+	.hsync_start = 800 + 40,
+	.hsync_end = 800 + 40 + 20,
+	.htotal = 800 + 40 + 20 + 40,
+	.vdisplay = 1280,
+	.vsync_start = 1280 + 16,
+	.vsync_end = 1280 + 16 + 9,
+	.vtotal = 1280 + 16 + 9 + 6,
+	.vrefresh = 60,
+	.flags = 0,
+	.width_mm = 95,
+	.height_mm = 152,
+};
+
+static inline struct jd9366 *panel_to_jd9366(struct drm_panel *panel)
+{
+	return container_of(panel, struct jd9366, panel);
+}
+
+static void jd9366_dcs_write_buf(struct jd9366 *ctx, const void *data,
+				  size_t len)
+{
+	struct mipi_dsi_device *dsi = to_mipi_dsi_device(ctx->dev);
+	int err;
+
+	err = mipi_dsi_dcs_write_buffer(dsi, data, len);
+	if (err < 0)
+		DRM_ERROR_RATELIMITED("MIPI DSI DCS write buffer failed: %d\n",
+				      err);
+}
+
+static void jd9366_dcs_write_cmd(struct jd9366 *ctx, u8 cmd, u8 value)
+{
+	struct mipi_dsi_device *dsi = to_mipi_dsi_device(ctx->dev);
+	int err;
+
+	err = mipi_dsi_dcs_write(dsi, cmd, &value, 1);
+	if (err < 0)
+		DRM_ERROR_RATELIMITED("MIPI DSI DCS write failed: %d\n", err);
+}
+
+#define dcs_write_seq(ctx, seq...)				\
+({								\
+	static const u8 d[] = { seq };				\
+								\
+	jd9366_dcs_write_buf(ctx, d, ARRAY_SIZE(d));		\
+})
+
+/*
+ * This panel is not able to auto-increment all cmd addresses so for some of
+ * them, we need to send them one by one...
+ */
+#define dcs_write_cmd_seq(ctx, cmd, seq...)			\
+({								\
+	static const u8 d[] = { seq };				\
+	unsigned int i;						\
+								\
+	for (i = 0; i < ARRAY_SIZE(d) ; i++)			\
+		jd9366_dcs_write_cmd(ctx, cmd + i, d[i]);	\
+})
+
+//E-Touch panel sequence provided by them:
+static void jd9366_init_sequence(struct jd9366 *ctx)
+{
+	dcs_write_seq(ctx, 0xE0,0x00);
+
+	dcs_write_seq(ctx, 0xE1,0x93);
+	dcs_write_seq(ctx, 0xE2,0x65);
+	dcs_write_seq(ctx, 0xE3,0xF8);
+	dcs_write_seq(ctx, 0x80,0x03); //0x01 MIPI 2 LANE  03 4 lane
+
+	dcs_write_seq(ctx, 0xE0,0x04);
+	dcs_write_seq(ctx, 0x2D,0x03);
+
+
+	dcs_write_seq(ctx, 0xE0,0x01);
+
+	dcs_write_seq(ctx, 0x00,0x00);
+	dcs_write_seq(ctx, 0x01,0x8E);
+	dcs_write_seq(ctx, 0x03,0x00);
+	dcs_write_seq(ctx, 0x04,0x8E);
+
+
+	dcs_write_seq(ctx, 0x17,0x00);
+	dcs_write_seq(ctx, 0x18,0xD8);
+	dcs_write_seq(ctx, 0x19,0x01);
+	dcs_write_seq(ctx, 0x1A,0x00);
+	dcs_write_seq(ctx, 0x1B,0xD8);
+	dcs_write_seq(ctx, 0x1C,0x01);
+
+	dcs_write_seq(ctx, 0x1F,0x74);
+	dcs_write_seq(ctx, 0x20,0x19);
+	dcs_write_seq(ctx, 0x21,0x19);
+	dcs_write_seq(ctx, 0x22,0x0E);
+
+
+	dcs_write_seq(ctx, 0x37,0x29);
+	dcs_write_seq(ctx, 0x38,0x05);
+	dcs_write_seq(ctx, 0x39,0x08);
+	dcs_write_seq(ctx, 0x3A,0x18);
+	dcs_write_seq(ctx, 0x3B,0x18);
+	dcs_write_seq(ctx, 0x3C,0x72);
+	dcs_write_seq(ctx, 0x3D,0xFF);
+	dcs_write_seq(ctx, 0x3E,0xFF);
+	dcs_write_seq(ctx, 0x3F,0xFF);
+
+
+	dcs_write_seq(ctx, 0x40,0x06);
+	dcs_write_seq(ctx, 0x41,0xA0);
+
+	dcs_write_seq(ctx, 0x43,0x10);
+	dcs_write_seq(ctx, 0x44,0x0E);
+	dcs_write_seq(ctx, 0x45,0x3C);
+
+	dcs_write_seq(ctx, 0x55,0x01);
+	dcs_write_seq(ctx, 0x56,0x01);
+	dcs_write_seq(ctx, 0x57,0x65);//VCL=2.5V
+	dcs_write_seq(ctx, 0x58,0x0A);
+	dcs_write_seq(ctx, 0x59,0x0A);
+	dcs_write_seq(ctx, 0x5A,0x28);
+	dcs_write_seq(ctx, 0x5B,0x10);
+
+	dcs_write_seq(ctx, 0x5D,0x7C);
+	dcs_write_seq(ctx, 0x5E,0x60);
+	dcs_write_seq(ctx, 0x5F,0x4E);
+	dcs_write_seq(ctx, 0x60,0x40);
+	dcs_write_seq(ctx, 0x61,0x39);
+	dcs_write_seq(ctx, 0x62,0x28);
+	dcs_write_seq(ctx, 0x63,0x2A);
+	dcs_write_seq(ctx, 0x64,0x11);
+	dcs_write_seq(ctx, 0x65,0x27);
+	dcs_write_seq(ctx, 0x66,0x23);
+	dcs_write_seq(ctx, 0x67,0x21);
+	dcs_write_seq(ctx, 0x68,0x3D);
+	dcs_write_seq(ctx, 0x69,0x2B);
+	dcs_write_seq(ctx, 0x6A,0x33);
+	dcs_write_seq(ctx, 0x6B,0x26);
+	dcs_write_seq(ctx, 0x6C,0x24);
+	dcs_write_seq(ctx, 0x6D,0x18);
+	dcs_write_seq(ctx, 0x6E,0x0A);
+	dcs_write_seq(ctx, 0x6F,0x00);
+
+	dcs_write_seq(ctx, 0x70,0x7C);
+	dcs_write_seq(ctx, 0x71,0x60);
+	dcs_write_seq(ctx, 0x72,0x4E);
+	dcs_write_seq(ctx, 0x73,0x40);
+	dcs_write_seq(ctx, 0x74,0x39);
+	dcs_write_seq(ctx, 0x75,0x29);
+	dcs_write_seq(ctx, 0x76,0x2B);
+	dcs_write_seq(ctx, 0x77,0x12);
+	dcs_write_seq(ctx, 0x78,0x27);
+	dcs_write_seq(ctx, 0x79,0x24);
+	dcs_write_seq(ctx, 0x7A,0x21);
+	dcs_write_seq(ctx, 0x7B,0x3E);
+	dcs_write_seq(ctx, 0x7C,0x2B);
+	dcs_write_seq(ctx, 0x7D,0x33);
+	dcs_write_seq(ctx, 0x7E,0x26);
+	dcs_write_seq(ctx, 0x7F,0x24);
+	dcs_write_seq(ctx, 0x80,0x19);
+	dcs_write_seq(ctx, 0x81,0x0A);
+	dcs_write_seq(ctx, 0x82,0x00);
+
+
+	dcs_write_seq(ctx, 0xE0,0x02);
+
+	dcs_write_seq(ctx, 0x00,0x0E);
+	dcs_write_seq(ctx, 0x01,0x06);
+	dcs_write_seq(ctx, 0x02,0x0C);
+	dcs_write_seq(ctx, 0x03,0x04);
+	dcs_write_seq(ctx, 0x04,0x08);
+	dcs_write_seq(ctx, 0x05,0x19);
+	dcs_write_seq(ctx, 0x06,0x0A);
+	dcs_write_seq(ctx, 0x07,0x1B);
+	dcs_write_seq(ctx, 0x08,0x00);
+	dcs_write_seq(ctx, 0x09,0x1D);
+	dcs_write_seq(ctx, 0x0A,0x1F);
+	dcs_write_seq(ctx, 0x0B,0x1F);
+	dcs_write_seq(ctx, 0x0C,0x1D);
+	dcs_write_seq(ctx, 0x0D,0x1D);
+	dcs_write_seq(ctx, 0x0E,0x1D);
+	dcs_write_seq(ctx, 0x0F,0x17);
+	dcs_write_seq(ctx, 0x10,0x37);
+	dcs_write_seq(ctx, 0x11,0x1D);
+	dcs_write_seq(ctx, 0x12,0x1F);
+	dcs_write_seq(ctx, 0x13,0x1E);
+	dcs_write_seq(ctx, 0x14,0x10);
+	dcs_write_seq(ctx, 0x15,0x1D);
+
+	dcs_write_seq(ctx, 0x16,0x0F);
+	dcs_write_seq(ctx, 0x17,0x07);
+	dcs_write_seq(ctx, 0x18,0x0D);
+	dcs_write_seq(ctx, 0x19,0x05);
+	dcs_write_seq(ctx, 0x1A,0x09);
+	dcs_write_seq(ctx, 0x1B,0x1A);
+	dcs_write_seq(ctx, 0x1C,0x0B);
+	dcs_write_seq(ctx, 0x1D,0x1C);
+	dcs_write_seq(ctx, 0x1E,0x01);
+	dcs_write_seq(ctx, 0x1F,0x1D);
+	dcs_write_seq(ctx, 0x20,0x1F);
+	dcs_write_seq(ctx, 0x21,0x1F);
+	dcs_write_seq(ctx, 0x22,0x1D);
+	dcs_write_seq(ctx, 0x23,0x1D);
+	dcs_write_seq(ctx, 0x24,0x1D);
+	dcs_write_seq(ctx, 0x25,0x18);
+	dcs_write_seq(ctx, 0x26,0x38);
+	dcs_write_seq(ctx, 0x27,0x1D);
+	dcs_write_seq(ctx, 0x28,0x1F);
+	dcs_write_seq(ctx, 0x29,0x1E);
+	dcs_write_seq(ctx, 0x2A,0x11);
+	dcs_write_seq(ctx, 0x2B,0x1D);
+
+	dcs_write_seq(ctx, 0x2C,0x09);
+	dcs_write_seq(ctx, 0x2D,0x1A);
+	dcs_write_seq(ctx, 0x2E,0x0B);
+	dcs_write_seq(ctx, 0x2F,0x1C);
+	dcs_write_seq(ctx, 0x30,0x0F);
+	dcs_write_seq(ctx, 0x31,0x07);
+	dcs_write_seq(ctx, 0x32,0x0D);
+	dcs_write_seq(ctx, 0x33,0x05);
+	dcs_write_seq(ctx, 0x34,0x11);
+	dcs_write_seq(ctx, 0x35,0x1D);
+	dcs_write_seq(ctx, 0x36,0x1F);
+	dcs_write_seq(ctx, 0x37,0x1F);
+	dcs_write_seq(ctx, 0x38,0x1D);
+	dcs_write_seq(ctx, 0x39,0x1D);
+	dcs_write_seq(ctx, 0x3A,0x1D);
+	dcs_write_seq(ctx, 0x3B,0x18);
+	dcs_write_seq(ctx, 0x3C,0x38);
+	dcs_write_seq(ctx, 0x3D,0x1D);
+	dcs_write_seq(ctx, 0x3E,0x1E);
+	dcs_write_seq(ctx, 0x3F,0x1F);
+	dcs_write_seq(ctx, 0x40,0x01);
+	dcs_write_seq(ctx, 0x41,0x1D);
+
+	dcs_write_seq(ctx, 0x42,0x08);
+	dcs_write_seq(ctx, 0x43,0x19);
+	dcs_write_seq(ctx, 0x44,0x0A);
+	dcs_write_seq(ctx, 0x45,0x1B);
+	dcs_write_seq(ctx, 0x46,0x0E);
+	dcs_write_seq(ctx, 0x47,0x06);
+	dcs_write_seq(ctx, 0x48,0x0C);
+	dcs_write_seq(ctx, 0x49,0x04);
+	dcs_write_seq(ctx, 0x4A,0x10);
+	dcs_write_seq(ctx, 0x4B,0x1D);
+	dcs_write_seq(ctx, 0x4C,0x1F);
+	dcs_write_seq(ctx, 0x4D,0x1F);
+	dcs_write_seq(ctx, 0x4E,0x1D);
+	dcs_write_seq(ctx, 0x4F,0x1D);
+	dcs_write_seq(ctx, 0x50,0x1D);
+	dcs_write_seq(ctx, 0x51,0x17);
+	dcs_write_seq(ctx, 0x52,0x37);
+	dcs_write_seq(ctx, 0x53,0x1D);
+	dcs_write_seq(ctx, 0x54,0x1E);
+	dcs_write_seq(ctx, 0x55,0x1F);
+	dcs_write_seq(ctx, 0x56,0x00);
+	dcs_write_seq(ctx, 0x57,0x1D);
+
+	dcs_write_seq(ctx, 0x58,0x10);
+	dcs_write_seq(ctx, 0x59,0x00);
+	dcs_write_seq(ctx, 0x5A,0x00);
+	dcs_write_seq(ctx, 0x5B,0x10);
+	dcs_write_seq(ctx, 0x5C,0x00);
+	dcs_write_seq(ctx, 0x5D,0xD0);
+	dcs_write_seq(ctx, 0x5E,0x01);
+	dcs_write_seq(ctx, 0x5F,0x02);
+	dcs_write_seq(ctx, 0x60,0x60);
+	dcs_write_seq(ctx, 0x61,0x01);
+	dcs_write_seq(ctx, 0x62,0x02);
+	dcs_write_seq(ctx, 0x63,0x06);
+	dcs_write_seq(ctx, 0x64,0x6A);
+	dcs_write_seq(ctx, 0x65,0x55);
+	dcs_write_seq(ctx, 0x66,0x0F);
+	dcs_write_seq(ctx, 0x67,0xF7);
+	dcs_write_seq(ctx, 0x68,0x08);
+	dcs_write_seq(ctx, 0x69,0x08);
+	dcs_write_seq(ctx, 0x6A,0x6A);
+	dcs_write_seq(ctx, 0x6B,0x10);
+	dcs_write_seq(ctx, 0x6C,0x00);
+	dcs_write_seq(ctx, 0x6D,0x00);
+	dcs_write_seq(ctx, 0x6E,0x00);
+	dcs_write_seq(ctx, 0x6F,0x88);
+	dcs_write_seq(ctx, 0x70,0x00);
+	dcs_write_seq(ctx, 0x71,0x17);
+	dcs_write_seq(ctx, 0x72,0x06);
+	dcs_write_seq(ctx, 0x73,0x7B);
+	dcs_write_seq(ctx, 0x74,0x00);
+	dcs_write_seq(ctx, 0x75,0x80);
+	dcs_write_seq(ctx, 0x76,0x01);
+	dcs_write_seq(ctx, 0x77,0x5D);
+	dcs_write_seq(ctx, 0x78,0x18);
+	dcs_write_seq(ctx, 0x79,0x00);
+	dcs_write_seq(ctx, 0x7A,0x00);
+	dcs_write_seq(ctx, 0x7B,0x00);
+	dcs_write_seq(ctx, 0x7C,0x00);
+	dcs_write_seq(ctx, 0x7D,0x03);
+	dcs_write_seq(ctx, 0x7E,0x7B);
+
+	dcs_write_seq(ctx, 0xE0,0x04);
+	dcs_write_seq(ctx, 0x09,0x10);
+	dcs_write_seq(ctx, 0x0E,0x38);
+
+	dcs_write_seq(ctx, 0x2B,0x2B);
+	dcs_write_seq(ctx, 0x2D,0x03);
+	dcs_write_seq(ctx, 0x2E,0x44);
+
+	dcs_write_seq(ctx, 0xE0,0x00);
+
+	dcs_write_seq(ctx, 0xE6,0x02);
+	dcs_write_seq(ctx, 0xE7,0x06);
+	//SLP OUT
+	//SSD_CMD(0x11);  	// SLPOUT
+	dcs_write_seq(ctx, 0x11);
+	//Delayms(120);
+	msleep(120);
+	//DISP ON
+	//SSD_CMD(0x29);  	// DSPON
+	dcs_write_seq(ctx, 0x29);
+	//Delayms(5);
+	msleep(5);
+	//--- TE----//
+	dcs_write_seq(ctx, 0x35,0x00);
+}
+
+static int jd9366_disable(struct drm_panel *panel)
+{
+	struct jd9366 *ctx = panel_to_jd9366(panel);
+
+	if (!ctx->enabled)
+		return 0;
+
+	backlight_disable(ctx->backlight);
+
+	ctx->enabled = false;
+
+	return 0;
+}
+
+static int jd9366_unprepare(struct drm_panel *panel)
+{
+	struct jd9366 *ctx = panel_to_jd9366(panel);
+	struct mipi_dsi_device *dsi = to_mipi_dsi_device(ctx->dev);
+	int ret;
+
+	if (!ctx->prepared)
+		return 0;
+
+	ret = mipi_dsi_dcs_set_display_off(dsi);
+	if (ret)
+		DRM_WARN("failed to set display off: %d\n", ret);
+
+	ret = mipi_dsi_dcs_enter_sleep_mode(dsi);
+	if (ret)
+		DRM_WARN("failed to enter sleep mode: %d\n", ret);
+
+	msleep(120);
+
+	if (ctx->reset_gpio) {
+		gpiod_set_value_cansleep(ctx->reset_gpio, 1);
+		msleep(20);
+	}
+
+	regulator_disable(ctx->supply);
+
+	ctx->prepared = false;
+
+	return 0;
+}
+
+static int jd9366_prepare(struct drm_panel *panel)
+{
+	struct jd9366 *ctx = panel_to_jd9366(panel);
+	struct mipi_dsi_device *dsi = to_mipi_dsi_device(ctx->dev);
+	int ret;
+	printk(KERN_NOTICE "BILL: etouchDRIVER: starting prepare...\n");
+	if (ctx->prepared)
+		return 0;
+
+	ret = regulator_enable(ctx->supply);
+	if (ret < 0) {
+		DRM_ERROR("failed to enable supply: %d\n", ret);
+		return ret;
+	}
+	printk(KERN_NOTICE "BILL: etouchDRIVER: prepare - regulator enabled...\n");
+	if (ctx->reset_gpio) {
+		gpiod_set_value_cansleep(ctx->reset_gpio, 1);
+		msleep(20);
+		gpiod_set_value_cansleep(ctx->reset_gpio, 0);
+		msleep(100);
+	}
+	printk(KERN_NOTICE "BILL: etouchDRIVER: prepare - reset pulse sent...\n");
+
+	jd9366_init_sequence(ctx);
+	printk(KERN_NOTICE "BILL: etouchDRIVER: prepare - init sequence sent...\n");
+	ret = mipi_dsi_dcs_exit_sleep_mode(dsi);
+	if (ret)
+		return ret;
+
+	msleep(125);
+
+	ret = mipi_dsi_dcs_set_display_on(dsi);
+	if (ret)
+		return ret;
+
+	msleep(20);
+
+	ctx->prepared = true;
+	printk(KERN_NOTICE "BILL: etouchDRIVER: prepare - exiting prepare successfully...\n");
+	return 0;
+}
+
+static int jd9366_enable(struct drm_panel *panel)
+{
+	struct jd9366 *ctx = panel_to_jd9366(panel);
+
+	if (ctx->enabled)
+		return 0;
+	printk(KERN_NOTICE "BILL: etouchDRIVER: inside enable - calling backlight enable...\n");
+	backlight_enable(ctx->backlight);
+
+	ctx->enabled = true;
+
+	return 0;
+}
+
+static int jd9366_get_modes(struct drm_panel *panel)
+{
+	struct drm_display_mode *mode;
+	printk(KERN_NOTICE "BILL: etouchDRIVER: get_modes - starting...\n");
+	mode = drm_mode_duplicate(panel->drm, &default_mode);
+	if (!mode) {
+		DRM_ERROR("failed to add mode %ux%ux@%u\n",
+			  default_mode.hdisplay, default_mode.vdisplay,
+			  default_mode.vrefresh);
+		return -ENOMEM;
+	}
+
+	drm_mode_set_name(mode);
+	printk(KERN_NOTICE "BILL: etouchDRIVER: modes name set...\n");
+	mode->type = DRM_MODE_TYPE_DRIVER | DRM_MODE_TYPE_PREFERRED;
+	drm_mode_probed_add(panel->connector, mode);
+	printk(KERN_NOTICE "BILL: etouchDRIVER: modes added - end of get_modes...\n");
+	panel->connector->display_info.width_mm = mode->width_mm;
+	panel->connector->display_info.height_mm = mode->height_mm;
+
+	return 1;
+}
+
+static const struct drm_panel_funcs jd9366_drm_funcs = {
+	.disable = jd9366_disable,
+	.unprepare = jd9366_unprepare,
+	.prepare = jd9366_prepare,
+	.enable = jd9366_enable,
+	.get_modes = jd9366_get_modes,
+};
+
+static int jd9366_probe(struct mipi_dsi_device *dsi)
+{
+	struct device *dev = &dsi->dev;
+	struct jd9366 *ctx;
+	int ret;
+	printk(KERN_NOTICE "BILL: etouchDRIVER: entering probe...\n");
+	ctx = devm_kzalloc(dev, sizeof(*ctx), GFP_KERNEL);
+	if (!ctx)
+		return -ENOMEM;
+
+	ctx->reset_gpio = devm_gpiod_get_optional(dev, "reset", GPIOD_OUT_HIGH_OPEN_DRAIN);
+	if (IS_ERR(ctx->reset_gpio)) {
+		ret = PTR_ERR(ctx->reset_gpio);
+		dev_err(dev, "cannot get reset GPIO: %d\n", ret);
+		return ret;
+	}
+	printk(KERN_NOTICE "BILL: etouchDRIVER: probe got gpio for reset...\n");
+	ctx->supply = devm_regulator_get(dev, "power");
+	if (IS_ERR(ctx->supply)) {
+		ret = PTR_ERR(ctx->supply);
+		dev_err(dev, "cannot get regulator: %d\n", ret);
+		return ret;
+	}
+	printk(KERN_NOTICE "BILL: etouchDRIVER: probe got regulator...\n");
+	ctx->backlight = devm_of_find_backlight(dev);
+	if (IS_ERR(ctx->backlight))
+		return PTR_ERR(ctx->backlight);
+
+	mipi_dsi_set_drvdata(dsi, ctx);
+	printk(KERN_NOTICE "BILL: etouchDRIVER: probe set drvdata...\n");
+	ctx->dev = dev;
+
+	dsi->lanes = 4;
+	dsi->format = MIPI_DSI_FMT_RGB888;
+	dsi->mode_flags = MIPI_DSI_MODE_VIDEO | MIPI_DSI_MODE_VIDEO_BURST |
+			  MIPI_DSI_MODE_LPM;
+
+	drm_panel_init(&ctx->panel);
+	ctx->panel.dev = dev;
+	ctx->panel.funcs = &jd9366_drm_funcs;
+
+	drm_panel_add(&ctx->panel);
+	printk(KERN_NOTICE "BILL: etouchDRIVER: probe panel added...\n");
+	ret = mipi_dsi_attach(dsi);
+	if (ret < 0) {
+		dev_err(dev, "mipi_dsi_attach() failed: %d\n", ret);
+		drm_panel_remove(&ctx->panel);
+		return ret;
+	}
+	printk(KERN_NOTICE "BILL: etouchDRIVER: probe panel attached - end of probe...\n");
+	return 0;
+}
+
+static int jd9366_remove(struct mipi_dsi_device *dsi)
+{
+	struct jd9366 *ctx = mipi_dsi_get_drvdata(dsi);
+
+	mipi_dsi_detach(dsi);
+	drm_panel_remove(&ctx->panel);
+
+	return 0;
+}
+
+static const struct of_device_id etouch_jd9365_of_match[] = {
+	{ .compatible = "etouch,jd9365" },
+	{ }
+};
+MODULE_DEVICE_TABLE(of, etouch_jd9365_of_match);
+
+static struct mipi_dsi_driver etouch_jd9365_driver = {
+	.probe = jd9366_probe,
+	.remove = jd9366_remove,
+	.driver = {
+		.name = "panel-etouch-jd9365",
+		.of_match_table = etouch_jd9365_of_match,
+	},
+};
+module_mipi_dsi_driver(etouch_jd9365_driver);
+
+MODULE_AUTHOR("Bill Hewlett <bill@imagecue.lighting");
+MODULE_DESCRIPTION("DRM Driver for E-Touch JD9365 MIPI DSI panel");
+MODULE_LICENSE("GPL v2");
